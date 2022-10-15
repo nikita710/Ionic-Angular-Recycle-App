a470e8180ccda7a82d81025afac92786ae3a4180
nikita710 <nikita.parmar721@gmail.com>
2022-10-15 12:15:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-recycle-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
